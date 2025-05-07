9b67801183442fb678ec02bef84dda68f0fceadc
Leogeox <leovginx@gmail.com>
2025-05-07 17:22:18 +0200

    Add files via upload

--- js/accueil.js
+++ js/accueil.js
@@ -1,12 +1,29 @@
-let sidenav = document.querySelector('.sidenav');
-let openSideNav = document.querySelector('.openSideNav');
+let blindbtn = document.getElementById('blindbtn');  
+let body = document.getElementById('body');
 
-openSideNav.addEventListener('click', function(){
-    if (sidenav.classList.contains('sidenav')) {
-        sidenav.classList.remove('sidenav');
-        sidenav.classList.add('sidenavOpen');
-    } else {
-        sidenav.classList.add('sidenav');
-        sidenav.classList.remove('sidenavOpen');
-    }
-});
+blindbtn.addEventListener('click', function(){
+    body.classList.toggle('blind')
+})
+
+
+let contents = document.querySelectorAll('.content')
+let tabs = document.querySelectorAll('.activity')
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', function(){
+        tabs.forEach((tab) => {
+            tab.classList.remove('tabActive')}) 
+        this.classList.add('tabActive')
+        contents.forEach((content) => {
+            content.classList.remove('active')
+            content.classList.add('content')
+        })  
+        if(this.classList.contains('tab_1')) {
+            contents[0].classList.add('active')
+            contents[0].classList.remove('content') 
+        }else if (this.classList.contains('tab_2')) {
+            contents[1].classList.add('active');
+            contents[1].classList.remove('content')
+        }
+    })
+})
